0c18e7a4b3c70a99e51fc942dd744cf3a51ce6aa
Xetera <mythologyali@gmail.com>
2018-12-01 00:16:55 -0800

    changed list to intsets holy fuck this is actually incredible I can't believe the optimization on this wtf

--- 2018/day1.hs
+++ 2018/day1.hs
@@ -1,5 +1,4 @@
-import Data.List
-import Debug.Trace 
+import qualified Data.IntSet as IntSet
 
 processItems = foldr (+) 0
 
@@ -7,19 +6,17 @@ replace ('+':num) = num
 replace any = any
 
 -- This shit literally took over 2 minutes to find the number
-findRepeats :: ([Int], Int) -> [Int] -> [Int]
 findRepeats (seen, current) nums = do
   let item = current + head nums
-  if item `elem` seen 
-    then item : findRepeats (item:seen, item) (tail nums)
-    else findRepeats (item:seen, item) (tail nums)
+  if item `IntSet.member` seen 
+    then item : findRepeats (item `IntSet.insert` seen, item) (tail nums)
+    else findRepeats (item `IntSet.insert` seen, item) (tail nums)
 
 main = do 
   strings <- map replace . lines <$> readFile "inputs/day1.txt"
   let numbers = map (\n -> read n :: Int) $ strings
   let solution1 = processItems numbers
 
-  let dupe = head $ findRepeats ([], 0) (cycle numbers)
-  return dupe
-  -- head . fst dupes
+  let dupes = findRepeats (IntSet.fromList [], 0) (cycle numbers)
+  return $ take 100 $ dupes
 
