a85618b9a48bc65800a95a480b6396dfc69f0f52
Xetera <mythologyali@gmail.com>
2018-12-01 23:07:01 -0800

    day2 part2 solution

--- 2018/day2.hs
+++ 2018/day2.hs
@@ -1,4 +1,6 @@
 import qualified Data.Map.Strict as Map (Map(..), insert, empty, lookup, filter, size)
+import Data.List
+import Data.Maybe
 
 items = lines <$> readFile "inputs/day2.txt"
 
@@ -10,8 +12,19 @@ occuranceCount line = foldr count (Map.empty) (line)
 
 hasFreq amount freq = Map.size (Map.filter (==amount) freq) > 0
 
+differenceCount :: String -> String -> Int
+differenceCount one two = length . filter (==False) $ zipWith (==) one two
+
+findCommon one two = map fst . filter (\t -> fst t == snd t) $ zip one two
+
 main = do
   freqs <- map occuranceCount <$> items
+  ids <- items
   let twos = filter (hasFreq 2) freqs
   let threes = filter (hasFreq 3) freqs
-  return $ length twos * length threes
+  let part1 = length twos * length threes
+  let (one:two:_) = filter (\one -> case find (\two -> differenceCount one two == 1) ids of
+        Just _ -> True
+        Nothing -> False
+        ) ids
+  return $ findCommon one two
