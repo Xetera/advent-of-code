c79810c0905203cb208b211812e714169e9f8e60
Xetera <mythologyali@gmail.com>
2019-12-02 01:09:02 -0800

    cleaned up day2

--- 2019/day2.hs
+++ 2019/day2.hs
@@ -3,24 +3,22 @@ import Data.List as L
 import Data.Vector as V
 import Text.ParserCombinators.ReadP
 
-nextCodes :: Vector Int -> Int -> (Int, Int, Int)
-nextCodes vector index =
-  (vector ! (index + 1), vector ! (index + 2), vector ! (index + 3))
-
 replaceAt :: (Int -> Int -> Int) -> Vector Int -> Int -> Vector Int
 replaceAt f vector index =
-  let (first, second, third) = nextCodes vector index
-   in vector // [(third, f (vector ! first) (vector ! second))]
+  vector // [(third, f (vector ! first) (vector ! second))]
+  where
+    nextCodes v idx = L.map (\i -> v ! (idx + i)) [1, 2, 3]
+    [first, second, third] = nextCodes vector index
 
 runOpcode :: Int -> Vector Int -> Int -> Vector Int
 runOpcode 1 = replaceAt (+)
 runOpcode 2 = replaceAt (*)
 runOpcode _ = error "you messed up"
 
-process :: Int -> V.Vector Int -> Vector Int
-process index nums
-  | nums ! index == 99 = nums
-  | otherwise = process (index + 4) newVector
+solve :: Int -> V.Vector Int -> Int
+solve index nums
+  | nums ! index == 99 = V.head nums
+  | otherwise = solve (index + 4) newVector
   where
     opcode = nums ! index
     newVector = runOpcode opcode nums index
@@ -36,19 +34,16 @@ opcodes = V.fromList . fst . L.last . readP_to_S (parseInt `sepBy1` char ',')
 choices :: [(Int, Int)]
 choices = [(x, y) | x <- [0 .. 99], y <- [0 .. 99]]
 
-answer :: V.Vector Int -> Int
-answer = V.head
-
 main :: IO ()
 main = do
   numbersStr <- readFile "inputs/day2.txt"
   let vector = opcodes numbersStr
-  let run = process 0
-  print $ Prelude.length choices
+  let run = solve 0
   -- part 1 answer
-  print $ answer $ run $ prepare vector (12, 2)
-  let day2Target = 19690720
-  let pairs = L.find ((== day2Target) . answer . run . prepare vector) choices
+  print . run $ prepare vector (12, 2)
+  -- part 2 answer
+  let part2Target = 19690720
+  let pairs = L.find ((== part2Target) . run . prepare vector) choices
   case pairs of
     Just (x, y) -> print $ 100 * x + y
     Nothing -> print "No pairs found"
