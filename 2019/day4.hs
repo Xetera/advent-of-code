b84d4a2dd8964feafd9f1a5614725e116ed9ac7e
Xetera <mythologyali@gmail.com>
2019-12-03 22:39:46 -0800

    cleaned up day4

--- 2019/day4.hs
+++ 2019/day4.hs
@@ -1,11 +1,4 @@
-import Data.Char (isDigit)
 import Data.List
-import Text.ParserCombinators.ReadP
-
-parse :: String -> [Int]
-parse input = fst . last $ readP_to_S (parseInt `sepBy` (char '-')) input
-  where
-    parseInt = read <$> many1 (satisfy isDigit)
 
 dataM :: IO String
 dataM = readFile "inputs/day4.txt"
@@ -16,7 +9,7 @@ paired = zip <*> tail
 passwordSpecs :: [String -> Bool]
 passwordSpecs = [isSix, isIncreasing]
   where
-    isSix = (== 6) . length
+    isSix str = length str == 6
     isIncreasing = all (uncurry (<=)) . paired
 
 isValidWith :: (String -> Bool) -> String -> Bool
@@ -30,7 +23,7 @@ part2 = isValidWith $ any ((== 2) . length) . group
 
 main :: IO ()
 main = do
-  [low, high] <- parse <$> dataM
+  let (low, high) = (235741, 706948)
   let range = map show [low .. high]
   let solve f = print . length $ filter f range
   -- part 1
